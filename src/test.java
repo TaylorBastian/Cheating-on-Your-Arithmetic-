5b1b1d0e620bacb3f3555e76d4bf38476697692e
Taylor Bastian <taylorjb16@gmail.com>
2016-09-05 15:09:21 -0600

    Added Files

---
+++ src/test.java
@@ -0,0 +1,7 @@
+
+public class test {
+	public static void main (String[] args){
+		System.out.println("79 + 3 * (4 + 82 - 68) - 7 + 19*(179 + 21 + 10) / 7 + 18110389 * 56 * 11 + 2246");
+		System.out.println(79 + 3 * (4 + 82 - 68) - 7 + 19*(179 + 21 + 10) / 7 + 18110389 * 56 * 11 + 2246);
+	}
+}
